734da04955effb7c0a9d7b7d21033a55b4d0f7f3
UnluckyNinja <unluckyninja1994@gmail.com>
2012-12-12 10:58:41 +0800

    register SignsListener on plugin enable

--- SignPlus/src/com/github/UnluckyNinja/signplus/SignPlus.java
+++ SignPlus/src/com/github/UnluckyNinja/signplus/SignPlus.java
@@ -1,5 +1,6 @@
 package com.github.UnluckyNinja.signplus;
 
+import com.github.UnluckyNinja.signplus.listeners.SignsListener;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +13,7 @@ public class SignPlus extends JavaPlugin {
     public  void onEnable(){
         config = this.getConfig();
         config.options().copyDefaults(true);
+        getServer().getPluginManager().registerEvents(new SignsListener(this), this);
         getLogger().info("[OmniSigns] OmniSigns has been enabled.");
     }
     @Override
