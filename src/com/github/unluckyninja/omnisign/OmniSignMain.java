e61a34a8793e96cf0d4c1b45811f234ebe857cd4
UnluckyNinja <unluckyninja1994@gmail.com>
2014-01-21 02:58:56 +0800

    add dependency Vault

---
+++ src/com/github/unluckyninja/omnisign/OmniSignMain.java
@@ -0,0 +1,150 @@
+package com.github.unluckyninja.omnisign;
+
+import lib.PatPeter.SQLibrary.Database;
+import lib.PatPeter.SQLibrary.SQLite;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.InvalidPluginException;
+import org.bukkit.plugin.UnknownDependencyException;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OmniSignMain extends JavaPlugin {
+
+    private Configuration config;
+
+    private Database sql;
+
+    private SignsManager SM;
+
+
+    @Override
+    public void onEnable() {
+//        SM = SignsManager.getInstance(this);
+//        config = this.getConfig();
+//        config.options().copyDefaults(true);
+
+        checkDependency();
+        sql = new SQLite(Logger.getLogger("Minecraft"),
+                "[" + getDescription().getName() + "]",
+                this.getDataFolder().getAbsolutePath(),
+                getDescription().getName(),
+                ".sqlite");
+        if (sql.open()) {
+            try {
+                sql.query("create table NormalSign(id int primary key)");
+                info("create table succeeded.");
+            } catch (SQLException e) {
+                error("create table failed.", e);
+            }
+        }
+        info(getDescription().getName() + " has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        info(getDescription().getName() + " has been disabled.");
+    }
+
+    private void checkDependency() {
+        if (!this.getServer().getPluginManager().isPluginEnabled("SQLibrary")) {
+            try {
+                info("Downloadind dependecy: SQLibrary");
+                URL url = new URL("http://repo.dakanilabs.com/content/repositories/public/lib/PatPeter/SQLibrary/SQLibrary/maven-metadata.xml");
+                URLConnection urlConnection = url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(in);
+                final String version = doc.getElementsByTagName("latest").item(0).getTextContent();
+                in.close();
+                url = new URL("http://repo.dakanilabs.com/content/repositories/public/lib/PatPeter/SQLibrary/SQLibrary/" + version + "/SQLibrary-" + version + ".jar");
+                final String path = this.getDataFolder().getParentFile().getAbsoluteFile() + "/SQLibrary-" + version + ".jar";
+                ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+                FileOutputStream fos = new FileOutputStream(path);
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                info("Finished downloading SQLibrary-" + version + ". Loading dependecy");
+                this.getServer().getPluginManager().loadPlugin(new File(path));
+                info("Loaded SQLibrary");
+            } catch (MalformedURLException ex) {
+                error("Failed to download dependency", ex);
+            } catch (IOException ex) {
+                error("Failed to download dependency", ex);
+            } catch (ParserConfigurationException ex) {
+                error("Failed to download dependency", ex);
+            } catch (SAXException ex) {
+                error("Failed to download dependency", ex);
+            } catch (InvalidPluginException ex) {
+                error("Failed to load dependency", ex);
+            } catch (InvalidDescriptionException ex) {
+                error("Failed to load dependency", ex);
+            } catch (UnknownDependencyException ex) {
+                error("Failed to load dependency", ex);
+            }
+        }
+
+        if (!this.getServer().getPluginManager().isPluginEnabled("Vault")) {
+            try {
+                info("Downloadind dependecy: Vault");
+                URL url = new URL("http://nexus.theyeticave.net/content/repositories/pub_releases/net/milkbowl/vault/Vault/maven-metadata.xml");
+                URLConnection urlConnection = url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(in);
+                final String version = doc.getElementsByTagName("release").item(0).getTextContent();
+                in.close();
+                url = new URL("http://nexus.theyeticave.net/content/repositories/pub_releases/net/milkbowl/vault/Vault/" + version + "/Vault-" + version + ".jar");
+                final String path = this.getDataFolder().getParentFile().getAbsoluteFile() + "/Vault-" + version + ".jar";
+                ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+                FileOutputStream fos = new FileOutputStream(path);
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                info("Finished downloading Vault-" + version + ". Loading dependecy");
+                this.getServer().getPluginManager().loadPlugin(new File(path));
+                info("Loaded Vault");
+            } catch (MalformedURLException ex) {
+                error("Failed to download dependency", ex);
+            } catch (IOException ex) {
+                error("Failed to download dependency", ex);
+            } catch (ParserConfigurationException ex) {
+                error("Failed to download dependency", ex);
+            } catch (SAXException ex) {
+                error("Failed to download dependency", ex);
+            } catch (InvalidPluginException ex) {
+                error("Failed to load dependency", ex);
+            } catch (InvalidDescriptionException ex) {
+                error("Failed to load dependency", ex);
+            } catch (UnknownDependencyException ex) {
+                error("Failed to load dependency", ex);
+            }
+        }
+    }
+
+    public void info(String string) {
+        getLogger().info(string);
+    }
+
+    public void error(String string, Throwable t) {
+        getLogger().log(Level.SEVERE, string, t);
+    }
+
+    public SignsManager getSignsManager() {
+        return SM;
+    }
+}
